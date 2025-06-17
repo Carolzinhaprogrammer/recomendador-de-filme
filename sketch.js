dc6fd02197565cca743f9e4ec8028f1d45c29c9a
Carolzinhaprogrammer <caroline.ouriques.ribas@escola.pr.gov.br>
2025-06-17 09:35:01 -0300

    criei o site

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("white");
+  let idade = campoIdade.value()
